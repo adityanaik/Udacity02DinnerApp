4454cbf8ca217b6f382c4af5d184d4ac18f1078b
Aditya Naik <adityanaik@outlook.com>
2015-08-15 21:47:12 +0530

    Display the value from the containerHolder key

--- app/src/main/java/com/example/android/dinnerapp/ShowDailySpecialActivity.java
+++ app/src/main/java/com/example/android/dinnerapp/ShowDailySpecialActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.tagmanager.ContainerHolder;
+
 /**
  * Created by Aditya on 13/8/2015.
  */
 public class ShowDailySpecialActivity extends Activity {
     String selectedDinnerExtrasKey = String.valueOf(R.string.selected_dinner);
-    public String mDailySpecial = "Fried egg with kit kat rashers";
+    public String mDailySpecial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,10 @@ public class ShowDailySpecialActivity extends Activity {
 
         // Get the text view we are using to show today' special
         TextView textViewTodaysSpecial = (TextView) findViewById(R.id.textView_todays_special);
-        // Set the text, currently hard wire it to a constant string
+
+        updateDailySpecial();
+
+        // Set the text to mDailySpecial
         textViewTodaysSpecial.setText(mDailySpecial);
     }
 
@@ -29,5 +34,16 @@ public class ShowDailySpecialActivity extends Activity {
         Intent intent = new Intent(this, OrderDinnerActivity.class);
         intent.putExtra(selectedDinnerExtrasKey, mDailySpecial);
         startActivity(intent);
+    }
+
+    /**
+     * Updates the value of mDailySpecial to that of containerHolder
+     */
+    public void updateDailySpecial() {
+        // Get the containerHolder
+        ContainerHolder containerHolder = ((MyApp) getApplication()).getmContainerHolder();
+        // Get the value of the "daily-special" key from the containerHolder
+        // The keys need to match exactly the keys you set in the Tag Manager
+        mDailySpecial = containerHolder.getContainer().getString("daily-special");
     }
 }
